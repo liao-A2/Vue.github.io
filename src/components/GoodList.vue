381f30e9f7eb1f8b310904310d5ccd1b5741bb2b
liao-A2 <61679945+liao-A2@users.noreply.github.com>
2020-04-28 00:27:34 +0800

    Vue.js

---
+++ src/components/GoodList.vue
@@ -0,0 +1,71 @@
+<template>
+    <div name="show">
+        <ul class="goodList">
+            <li  v-for="goods in list" :key="goods">
+                <img v-bind:src="goods.img">
+                <p>{{goods.goodName}} </p>
+            </li>
+        </ul>
+    </div>
+</template>
+<style type="text/css">
+    .goodList li{
+        width: 200px;
+        height: 200px;
+        list-style: none;
+        float: left;
+        font-size: 15px;
+        color: red;
+        margin-bottom: 30px;
+    }
+    .goodList img{
+        width: 180px;
+        height: 180px;
+    }
+</style>
+
+<script type="text/javascript">
+    export default{
+        name:"show",
+        data(){
+            var _this=this;
+            var url="";
+             if(_this.goodId==1){
+                    url="json/bijiben.json";
+             }else if(_this.goodId==2){
+                    url="json/shouji.json";
+             }else{
+                    url="json/bijiben.json";
+             }
+            this.$http.get(url).then(function(res){
+                _this.list=res.data;
+            })
+            return{
+                list:[]
+            }
+        },
+        props:{
+            goodId:Number
+        },
+        watch:{
+            goodId(){
+                var _this=this;
+                var url="";
+                if(_this.goodId==1){
+                    url="json/bijiben.json";
+                }else if(_this.goodId==2){
+                    url="json/shouji.json";
+                }else{
+                    url="json/bijiben.json";
+                }
+                    this.$http.get(url).then(function(res){
+                     _this.list=res.data;
+                 })
+                  return{
+                      list:[]
+                 }
+            }
+        }
+    }
+</script>
+
---
+++ src/views/Main.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="main">
+        <div class="left">
+            <Left></Left>
+        </div>
+        <div class="right">
+            <div class="top">
+                <img src="img/top.jpg" >
+            </div>
+            <div class="buttom">
+                <Right></Right>
+            </div>
+        </div>
+    </div>
+</template>
+<style type="textcss/">
+    *{
+        padding: 0;
+        margin: 0;
+    }
+    .left{
+        width: 100px;
+        float: left;
+        margin-right: 10px;
+    }
+    .right{
+        width: 1000px;
+        float: left;
+        margin-left: 10px;
+    }
+    .main{
+        width: 1200px;
+        margin:20px auto;
+    }
+    .top img{
+        height: 200px;
+        width: 1000px;
+    }
+    .left,.right{
+        background: #f5f5f5;
+        
+    }
+</style>
+<script type="text/javascript">
+    import Left from "../components/Left.vue"
+    import Right from "../components/Right.vue"
+    export default{
+        components:{
+            Left,
+            Right
+        }
+    }
+</script>
+
